01521c45d0d20730a264fb474ea69590b8b767db
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2020-01-17 17:21:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+const sans= document.querySelectorAll('h1 span')
+span.forEach(span => span.addEventListener('mouseover', function(e) {
+  span.classList.add('animated', 'rubberBand')
+}))
+spans.forEach(span => span.addEventListener('mouseout', function (e)  {
+  span.classList.remove('animated', 'rubberBand')
+}))
 
+const htmlBar= document.querySelector('.bar-html')
+const cssBar = document.querySelector('.bar-css')
+const jsBar = document.querySelector('.bar-javascript') 
+const reactBar = document.querySelector('.bar-javascript')
+var t1 = new TimelineLite()
+
+t1.fromTo(htmlBar, .75, {width: 'calc(0% - 6px)'}, { width: 'calc(90% - 6px)', ease: Power4.easeOut})
+.fromTo(cssBar, .75, {width: 'calc(0% - 6px)'}, { width: 'calc(90% - 6px)', ease: Power4.easeOut})
+.fromTo(jsBar, .75, {width: 'calc(0% - 6px)'}, { width: 'calc(90% - 6px)', ease: Power4.easeOut})
+.fromTo(reactBar, .75, {width: 'calc(0% - 6px)'}, { width: 'calc(90% - 6px)', ease: Power4.easeOut})
+
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+  triggerElement: '.skills',
+  triggerHook: 0
+})
+.setTwee(t1)
+.addTo(controller)
+
+const showRequiredCategory= event => {
+  const getId= event.id
+  const link = document.querySelectorAll('.work-category button')
+for(i=0; i<links.lenth; i++){
+  if(inks[i].hashAttribute('class'))  {
+    links[i].classList.remove('active')
+  }
+}
+  event.classList.add('active')
+  const getCategory = document.querySelector('.category-$(getId)')
+  const categories= document.querySelectorAll('div[class ^= "category-"]')
+  for  (i=0; i<categories.length; i++)  {
+    if(categories[i].hasAttribute('class')) {
+    categories[i].classList.remove('showCategory')
+      categories[i].classList.remove('hideCategory')
+      
+          }
+          }
+          
+  getCategory.classList.remove('hideCategory')
+  getCategory.classList.add('showCategory')
+}
+  
